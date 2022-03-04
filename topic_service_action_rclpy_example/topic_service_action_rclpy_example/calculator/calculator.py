6fa855f767f9b7d3b6d8a023421e6fddb60bd4d7
Dongun-code <dongun@outlook.com>
2022-03-04 11:28:04 +0900

    "srv function done"

--- topic_service_action_rclpy_example/topic_service_action_rclpy_example/calculator/calculator.py
+++ topic_service_action_rclpy_example/topic_service_action_rclpy_example/calculator/calculator.py
@@ -14,6 +14,7 @@
 
 import time
 from msg_srv_action_interface_example.msg import ArithmeticArgument
+from msg_srv_action_interface_example.srv import ArithmeticOperator
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.node import Node
 from rclpy.qos import QoSDurabilityPolicy
@@ -23,33 +24,78 @@ from rclpy.qos import QoSReliabilityPolicy
 
 class Calculator(Node):
 
-  def __init__(self):
-    super().__init__('calculator')
-    self.argument_a = 0.0
-    self.argument_b = 0.0
+    def __init__(self):
+        super().__init__('calculator')
+        self.argument_a = 0.0
+        self.argument_b = 0.0
+        self.argument_operator = 0
+        self.argument_result = 0.0
+        self.argument_formula = ''
+        self.operator = ['+', '-', '*', '/']
+        self.callback_group = ReentrantCallbackGroup()
 
-    self.callback_group = ReentrantCallbackGroup()
+        self.declare_parameter('qos_depth', 10)
+        qos_depth = self.get_parameter('qos_depth').value
 
-    self.declare_parameter('qos_depth', 10)
-    qos_depth = self.get_parameter('qos_depth').value
+        QOS_RKL10V = QoSProfile(
+          reliability = QoSReliabilityPolicy.RELIABLE,
+          history = QoSHistoryPolicy.KEEP_LAST,
+          depth=qos_depth,
+          durability=QoSDurabilityPolicy.VOLATILE)
 
-    QOS_RKL10V = QoSProfile(
-      reliability = QoSReliabilityPolicy.RELIABLE,
-      history = QoSHistoryPolicy.KEEP_LAST,
-      depth=qos_depth,
-      durability=QoSDurabilityPolicy.VOLATILE)
+        self.arithmetic_argument_subscriber = self.create_subscription(
+          ArithmeticArgument,
+          'arithmetic_argument',
+          self.get_arithmetic_argument,
+          QOS_RKL10V,
+          callback_group=self.callback_group)
 
-    self.arithmetic_argument_subscriber = self.create_subscription(
-      ArithmeticArgument,
-      'arithmetic_argument',
-      self.get_arithmetic_argument,
-      QOS_RKL10V,
-      callback_group=self.callback_group)
+        self.arithmetic_service_server = self.create_service(
+            ArithmeticOperator,
+            'arithmetic_operator',
+            self.get_arithmetic_operator,
+            callback_group=self.callback_group)
 
-  def get_arithmetic_argument(self, msg):
-    self.argument_a = msg.argument_a
-    self.argument_b = msg.argument_b
-    self.get_logger().info('Timestamp of the message: {0}'.format(msg.stamp))
-    self.get_logger().info('Subscribed argument a: {0}'.format(self.argument_a))
-    self.get_logger().info('Subscribed argument b: {0}'.format(self.argument_b))
+    def get_arithmetic_argument(self, msg):
+        self.argument_a = msg.argument_a
+        self.argument_b = msg.argument_b
+        self.get_logger().info('Timestamp of the message: {0}'.format(msg.stamp))
+        self.get_logger().info('Subscribed argument a: {0}'.format(self.argument_a))
+        self.get_logger().info('Subscribed argument b: {0}'.format(self.argument_b))
+
+    def get_arithmetic_operator(self, request, response):
+        self.argument_operator = request.arithmetic_operator
+
+        self.argument_result = self.calculate_given_formula(
+            self.argument_a,
+            self.argument_b,
+            self.argument_operator)
+        response.arithmetic_result = self.argument_result
+        self.argument_formula = '{0} {1} {2} = {3}'.format(
+                self.argument_a,
+                self.operator[self.argument_operator-1],
+                self.argument_b,
+                self.argument_result)
+        self.get_logger().info(self.argument_formula)
+        return response
+
+    def calculate_given_formula(self, a , b, operator):
+        if operator == ArithmeticOperator.Request.PLUS:
+            self.argument_result = a + b
+        elif operator == ArithmeticOperator.Request.MINUS:
+            self.argument_result = a - b
+        elif operator == ArithmeticOperator.Request.MULTIPLY:
+            self.argument_result = a * b
+        elif operator == ArithmeticOperator.Request.DIVISION:
+            try:
+                self.argument_result = a / b
+            except ZeroDivisionError:
+                self.get_logger().error('ZeroDivision Error!')
+                self.argument_result = 0.0
+                return self.argument_result
+        else:
+            self.get_logger().error(
+                'Please make sure arithmetic operator(plus, minus, multiply, division).')
+            self.argument_result = 0.0
+        return self.argument_result
 
